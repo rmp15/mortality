a09b0761dffab7bc5f9ce3decd01d06396f6828f
Robbie Parks <r.parks15@imperial.ac.uk>
2016-09-20 17:51:46 +0100

    added file

--- USA/state/prog/adj_matrix_create/adj_matrix_fix.R
+++ USA/state/prog/adj_matrix_create/adj_matrix_fix.R
@@ -3,5 +3,7 @@ rm(list=ls())
 # load text file with adjacency matrix
 dat <- read.table('~/git/mortality/USA/state/output/adj_matrix_create/USA.graph',header=F, sep= ' ', col.names = (paste0('V',seq_len(10))),fill=T)
 
+# adjust the file to fix Hawaii and Alaska connections
+
 
 
