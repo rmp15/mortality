314ed04a9f6d90a1abcfa269e5918059476fa169
Robbie Parks <r.parks15@imperial.ac.uk>
2016-09-26 12:43:48 +0100

    tidied

--- USA/state/prog/format_mort/format_mort.R
+++ USA/state/prog/format_mort/format_mort.R
@@ -1,6 +1,7 @@
 rm(list=ls())
 
 library(foreign)
+library(readr)
 
 # arguments from Rscript
 args <- commandArgs(trailingOnly=TRUE)
@@ -9,178 +10,101 @@ args <- commandArgs(trailingOnly=TRUE)
 year <- as.numeric(args[1])
 file.type <- as.character(args[2])
 
-year <- 2011
-file.type <- 'USPART2'
+# file name
+file.name 	<- paste0('~/data/mortality/US/state/raw/cdc/',year,'/MULT',year,'.',file.type)
 
-# load file
-dat <- readLines(paste0('~/data/mortality/US/state/raw/cdc/',year,'/MULT',year,'.',file.type))
-
-# parse file
-dat.clean <- data.frame(rectype=NA,resident=NA,stateocc_fips=NA,countyocc_fips=NA,pop_countyocc=NA,
-			stateres_fips=NA,countyres_fips=NA,cityres_fips=NA,pop_cityres=NA,metro=NA,
-			ex_stateres=NA,pmsares_fips=NA,pop_countyres=NA,pop_pmsa=NA,cmsa_res=NA,
-			statebirth=NA,educ=NA,educ_recode=NA,monthdth=NA,sex=NA,
-			age_detail=NA,placedeath=NA,marital=NA,day_week=NA,year=NA,
-			injurywork=NA,mannerdth=NA,autopsy=NA,activcode=NA,place_injury=NA,
-			race_detail=NA,origin=NA,hispanic_recode=NA,cause=NA,num_entity=NA,
-			seqn_ent1=NA,cause_ent1=NA,seqn_ent2=NA,cause_ent2=NA,seqn_ent3=NA,
-			cause_ent3=NA,seqn_ent4=NA,cause_ent4=NA,seqn_ent5=NA,cause_ent5=NA,
-			seqn_ent6=NA,cause_ent6=NA,seqn_ent7=NA,cause_ent7=NA,seqn_ent8=NA,
-			cause_ent8=NA,seqn_ent9=NA,cause_ent9=NA,seqn_ent10=NA,cause_ent10=NA,
-			seqn_ent11=NA,cause_ent11=NA,seqn_ent12=NA,cause_ent12=NA,seqn_ent13=NA,
-			cause_ent13=NA,seqn_ent14=NA,cause_ent14=NA,seqn_ent15=NA,cause_ent15=NA,
-			seqn_ent16=NA,cause_ent16=NA,seqn_ent17=NA,cause_ent17=NA,seqn_ent18=NA,
-			cause_ent18=NA,seqn_ent19=NA,cause_ent19=NA,seqn_ent20=NA,cause_ent20=NA,
-			num_record=NA,cause_rec1=NA,cause_rec2=NA,cause_rec3=NA,cause_rec4=NA,
-			cause_rec5=NA,cause_rec6=NA,cause_rec7=NA,cause_rec8=NA,cause_rec9=NA,
-			cause_rec10=NA,cause_rec11=NA,cause_rec12=NA,cause_rec13=NA,cause_rec14=NA,
-			cause_rec15=NA,cause_rec16=NA,cause_rec17=NA,cause_rec18=NA,cause_rec19=NA,
-			cause_rec20=NA
+start.positions.1 <-   c(	19,20,21,23,28,
+			29,35,38,43,44,
+			45,47,51,52,53,
+			55,61,63,65,69,
+			70,83,84,85,102,
+			106,107,109,144,145,
+			#445,484,488,
+			146,163,
+			165,167,172,174,179,
+			181,186,188,193,195,
+			200,202,207,209,214,
+			216,221,223,228,230,
+			235,237,242,244,249,
+			251,256,258,263,265,
+			270,272,277,279,284,
+			286,291,293,298,300,
+			341,344,349,354,359,
+			364,369,374,379,384,
+			389,394,399,404,409,
+			414,419,424,429,434,
+			439			
 			)
 
-#for(i in c(1:length(dat))) {
-for(i in c(1:500)) {
-	rectype 	<- 	as.character(substr(dat[i],19,19))
-	resident 	<- 	as.character(substr(dat[i],20,20))
-	stateocc_fips 	<- 	as.character(substr(dat[i],21,22))
-	countyocc_fips 	<- 	as.character(substr(dat[i],23,25))
-	pop_countyocc 	<-	as.character(substr(dat[i],28,28))
+start.positions.2 <-   c(445,484,488)
 
-	stateres_fips 	<-	as.character(substr(dat[i],29,30))
-	countyres_fips 	<-	as.character(substr(dat[i],35,37))
-	cityres_fips 	<-	as.character(substr(dat[i],38,42))
-	pop_cityres	<-	as.character(substr(dat[i],43,43))
-	metro 		<-	as.character(substr(dat[i],44,44))
+end.positions.1 	<-   c(	19,20,22,25,28,
+			30,37,42,43,44,
+			46,50,51,52,54,
+			56,62,63,66,69,
+			73,83,84,85,105,
+			106,107,109,144,145,
+			#446,486,488,
+			149,164,
+			166,171,173,178,180,
+			185,187,192,194,199,
+			201,206,208,213,215,
+			220,222,227,229,234,
+			236,241,243,248,250,
+			255,257,262,264,269,
+			271,276,278,283,285,
+			290,292,297,299,304,
+			342,348,353,358,363,
+			368,373,378,383,388,
+			393,398,403,408,413,
+			418,423,428,433,438,
+			443
+			)
 
-	ex_stateres 	<-	as.character(substr(dat[i],45,46))
-	pmsares_fips 	<-	as.character(substr(dat[i],47,50))
-	pop_countyres 	<-	as.character(substr(dat[i],51,51))
-	pop_pmsa 	<-	as.character(substr(dat[i],52,52))
-	cmsa_res 	<-	as.character(substr(dat[i],53,54))
+end.positions.2 	<-   c(446,486,488)
 
-	statebirth 	<-	as.character(substr(dat[i],55,56))
-	educ 		<-	as.character(substr(dat[i],61,62))
-	educ_recode	<-	as.character(substr(dat[i],63,63))
-	monthdth 	<-	as.character(substr(dat[i],65,66))
-	sex 		<-	as.character(substr(dat[i],69,69))
-
-	age_detail 	<-	as.character(substr(dat[i],70,73))
-	placedeath 	<-	as.character(substr(dat[i],83,83))
-	marital 	<-	as.character(substr(dat[i],84,84))
-	day_week 	<-	as.character(substr(dat[i],85,85))
-	year 		<-	as.character(substr(dat[i],102,105))
-
-	injurywork 	<-	as.character(substr(dat[i],106,106))
-	mannerdth	<-	as.character(substr(dat[i],107,107))
-	autopsy 	<-	as.character(substr(dat[i],109,109))
-	activcode 	<-	as.character(substr(dat[i],144,144))
-	place_injury 	<-	as.character(substr(dat[i],145,145))
-
-	race_detail 	<-	as.character(substr(dat[i],445,446))
-	origin		<-	as.character(substr(dat[i],484,486))
-	hispanic_recode <-	as.character(substr(dat[i],488,488))
-	cause 		<-	as.character(substr(dat[i],146,149))
-	num_entity 	<-	as.character(substr(dat[i],163,164))
-
-	seqn_ent1 	<-	as.character(substr(dat[i],165,166))
-	cause_ent1	<-	as.character(substr(dat[i],167,171))
-	seqn_ent2 	<-	as.character(substr(dat[i],172,173))
-	cause_ent2 	<-	as.character(substr(dat[i],174,178))
-	seqn_ent3 	<-	as.character(substr(dat[i],179,180))
-
-	cause_ent3 	<-	as.character(substr(dat[i],181,185))
-	seqn_ent4	<-	as.character(substr(dat[i],186,187))
-	cause_ent4 	<-	as.character(substr(dat[i],188,192))
-	seqn_ent5 	<-	as.character(substr(dat[i],193,194))
-	cause_ent5 	<-	as.character(substr(dat[i],195,199))
-
-	seqn_ent6 	<-	as.character(substr(dat[i],200,201))
-	cause_ent6	<-	as.character(substr(dat[i],202,206))
-	seqn_ent7 	<-	as.character(substr(dat[i],207,208))
-	cause_ent7 	<-	as.character(substr(dat[i],209,213))
-	seqn_ent8 	<-	as.character(substr(dat[i],214,215))
-
-	cause_ent8 	<-	as.character(substr(dat[i],216,220))
-	seqn_ent9	<-	as.character(substr(dat[i],221,222))
-	cause_ent9 	<-	as.character(substr(dat[i],223,227))
-	seqn_ent10 	<-	as.character(substr(dat[i],228,229))
-	cause_ent10 	<-	as.character(substr(dat[i],230,234))
-
-	seqn_ent11 	<-	as.character(substr(dat[i],235,236))
-	cause_ent11	<-	as.character(substr(dat[i],237,241))
-	seqn_ent12 	<-	as.character(substr(dat[i],242,243))
-	cause_ent12 	<-	as.character(substr(dat[i],244,248))
-	seqn_ent13 	<-	as.character(substr(dat[i],249,250))
-
-	cause_ent13 	<-	as.character(substr(dat[i],251,255))
-	seqn_ent14	<-	as.character(substr(dat[i],256,257))
-	cause_ent14 	<-	as.character(substr(dat[i],258,262))
-	seqn_ent15 	<-	as.character(substr(dat[i],263,264))
-	cause_ent15 	<-	as.character(substr(dat[i],265,269))
-
-	seqn_ent16 	<-	as.character(substr(dat[i],270,271))
-	cause_ent16	<-	as.character(substr(dat[i],272,276))
-	seqn_ent17 	<-	as.character(substr(dat[i],277,278))
-	cause_ent17 	<-	as.character(substr(dat[i],279,283))
-	seqn_ent18 	<-	as.character(substr(dat[i],284,285))
-
-	cause_ent18 	<-	as.character(substr(dat[i],286,290))
-	seqn_ent19	<-	as.character(substr(dat[i],291,292))
-	cause_ent19 	<-	as.character(substr(dat[i],293,297))
-	seqn_ent20 	<-	as.character(substr(dat[i],298,299))
-	cause_ent20 	<-	as.character(substr(dat[i],300,304))
-
-	num_record 	<-	as.character(substr(dat[i],341,342))
-	cause_rec1	<-	as.character(substr(dat[i],344,348))
-	cause_rec2 	<-	as.character(substr(dat[i],349,353))
-	cause_rec3 	<-	as.character(substr(dat[i],354,358))
-	cause_rec4 	<-	as.character(substr(dat[i],359,363))
-
-	cause_rec5 	<-	as.character(substr(dat[i],364,368))
-	cause_rec6	<-	as.character(substr(dat[i],369,373))
-	cause_rec7 	<-	as.character(substr(dat[i],374,378))
-	cause_rec8	<-	as.character(substr(dat[i],379,383))
-	cause_rec9 	<-	as.character(substr(dat[i],384,388))
-
-	cause_rec10 	<-	as.character(substr(dat[i],389,393))
-	cause_rec11	<-	as.character(substr(dat[i],394,398))
-	cause_rec12 	<-	as.character(substr(dat[i],399,403))
-	cause_rec13	<-	as.character(substr(dat[i],404,408))
-	cause_rec14 	<-	as.character(substr(dat[i],409,413))
-
-	cause_rec15 	<-	as.character(substr(dat[i],414,418))
-	cause_rec16	<-	as.character(substr(dat[i],419,423))
-	cause_rec17 	<-	as.character(substr(dat[i],424,428))
-	cause_rec18	<-	as.character(substr(dat[i],429,433))
-	cause_rec19 	<-	as.character(substr(dat[i],434,438))
-
-	cause_rec20 	<-	as.character(substr(dat[i],439,443))
-
-	row <- c(rectype,resident,stateocc_fips,countyocc_fips,pop_countyocc,
-		stateres_fips,countyres_fips,cityres_fips,pop_cityres,metro,
-		ex_stateres,pmsares_fips,pop_countyres,pop_pmsa,cmsa_res,
-		statebirth,educ,educ_recode,monthdth,sex,
-		age_detail,placedeath,marital,day_week,year,
-		injurywork,mannerdth,autopsy,activcode,place_injury,
-		race_detail,origin,hispanic_recode,cause,num_entity,
-		seqn_ent1,cause_ent1,seqn_ent2,cause_ent2,seqn_ent3,
-		cause_ent3,seqn_ent4,cause_ent4,seqn_ent5,cause_ent5,
-		seqn_ent6,cause_ent6,seqn_ent7,cause_ent7,seqn_ent8,
-		cause_ent8,seqn_ent9,cause_ent9,seqn_ent10,cause_ent10,
-		seqn_ent11,cause_ent11,seqn_ent12,cause_ent12,seqn_ent13,
-		cause_ent13,seqn_ent14,cause_ent14,seqn_ent15,cause_ent15,
-		seqn_ent16,cause_ent16,seqn_ent17,cause_ent17,seqn_ent18,
-		cause_ent18,seqn_ent19,cause_ent19,seqn_ent20,cause_ent20,
-		num_record,cause_rec1,cause_rec2,cause_rec3,cause_rec4,
-		cause_rec5,cause_rec6,cause_rec7,cause_rec8,cause_rec9,
-		cause_rec10,cause_rec11,cause_rec12,cause_rec13,cause_rec14,
-		cause_rec15,cause_rec16,cause_rec17,cause_rec18,cause_rec19,
-		cause_rec20
+names.1 	<- c('rectype','resident','stateocc_fips','countyocc_fips','pop_countyocc',
+		'stateres_fips','countyres_fips','cityres_fips','pop_cityres','metro',
+		'ex_stateres','pmsares_fips','pop_countyres','pop_pmsa','cmsa_res',
+		'statebirth','educ','educ_recode','monthdth','sex',
+		'age_detail','placedeath','marital','day_week','year',
+		'injurywork','mannerdth','autopsy','activcode','place_injury',
+		#'race_detail','origin','hispanic_recode',
+		'cause','num_entity',
+		'seqn_ent1','cause_ent1','seqn_ent2','cause_ent2','seqn_ent3',
+		'cause_ent3','seqn_ent4','cause_ent4','seqn_ent5','cause_ent5',
+		'seqn_ent6','cause_ent6','seqn_ent7','cause_ent7','seqn_ent8',
+		'cause_ent8','seqn_ent9','cause_ent9','seqn_ent10','cause_ent10',
+		'seqn_ent11','cause_ent11','seqn_ent12','cause_ent12','seqn_ent13',
+		'cause_ent13','seqn_ent14','cause_ent14','seqn_ent15','cause_ent15',
+		'seqn_ent16','cause_ent16','seqn_ent17','cause_ent17','seqn_ent18',
+		'cause_ent18','seqn_ent19','cause_ent19','seqn_ent20','cause_ent20',
+		'num_record','cause_rec1','cause_rec2','cause_rec3','cause_rec4',
+		'cause_rec5','cause_rec6','cause_rec7','cause_rec8','cause_rec9',
+		'cause_rec10','cause_rec11','cause_rec12','cause_rec13','cause_rec14',
+		'cause_rec15','cause_rec16','cause_rec17','cause_rec18','cause_rec19',
+		'cause_rec20'
 		)
-	dat.clean <- rbind(dat.clean,row)
-	print(paste0('row',i))
-}
-dat.clean <- dat.clean[-1,]
 
-write.dta(dat.clean,paste0('~/data/mortality/US/state/raw/cdc/',year,'/MULT',year,'.',file.type,'.processed.dta'))
-write.csv(dat.clean,paste0('~/data/mortality/US/state/raw/cdc/',year,'/MULT',year,'.',file.type,'.processed.csv'))
+names.2		<- c('race_detail','origin','hispanic_recode')
+
+# load file part 1
+dat.1	 	<- 	readr::read_fwf(file.name, fwf_positions(start.positions.1,end.positions.1,names.1), progress=interactive())
+
+# establish any problems in data extraction
+problems.1 <- problems(dat.1)
+problem.rows <- sort(unique(problems.1$row))
+dat.problem  <- dat.1[problem.rows,]
+write.csv(dat.problem, paste0('~/data/mortality/US/state/raw/cdc/',year,'/MULT',year,'.',file.type,'.problems.csv'))
+
+# load file part 2
+dat.2	 	<- 	readr::read_fwf(file.name, fwf_positions(start.positions.2,end.positions.2,names.2), progress=interactive())
+
+# bind two parts of file
+dat <- cbind(dat.1,dat.2)
+
+# reorder to kyle/sandeep order
+dat <- dat[,c(1:30,94:96,31:93)]
+
+write.dta(dat,paste0('~/data/mortality/US/state/raw/cdc/',year,'/MULT',year,'.',file.type,'.processed.dta'))
+write.csv(dat,paste0('~/data/mortality/US/state/raw/cdc/',year,'/MULT',year,'.',file.type,'.processed.csv'))
