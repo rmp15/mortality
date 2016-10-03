3469098621b18f2b04924324a2bb979cf6e67204
Robbie Parks <r.parks15@imperial.ac.uk>
2016-10-03 17:25:02 +0100

    added plots

--- USA/state/prog/00_bash/plotting_results.sh
+++ USA/state/prog/00_bash/plotting_results.sh
@@ -8,23 +8,32 @@ clear
 declare -a ages=(0 5 15 25 35 45 55 65 75 85)
 declare -i model=2
 declare -i start=1982
-declare -i end=2013
+declare -i end=2010
 declare country="USA"
 
+# this will disappear once sorted out
+Rscript ~/git/mortality/USA/state/prog/mapping_posterior/mapping_posterior.R $start $end $country $model
+
 #################################################
-# 1. PLOT 1
+# 1. PREPARE DATA FOR PLOTTING
+#################################################
+
+# DATA PREPARATION
+
+#################################################
+# 2. PLOT 1
 #################################################
 
 # PLOT SOMETHING
 
 #################################################
-# 2. PLOT 2
+# 3. PLOT 2
 #################################################
 
 # PLOT SOMETHING ELSE
 
 #################################################
-# 3. PLOT 3
+# 4. PLOT 3
 #################################################
 
 # PLOT SOMETHING ELSE AGAIN
