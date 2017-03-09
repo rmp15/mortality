3e61393f8477e734596418cda8cd058fea5a9e16
rmp15 <r.parks15@imperial.ac.uk>
2017-03-09 22:34:38 +0000

    Attempt at Rossby wave calculations

--- USA/state/prog/rossby/rossby.R
+++ USA/state/prog/rossby/rossby.R
@@ -7,3 +7,18 @@ year.end <- as.numeric(args[2])
 
 # add sourced data
 source('../../data/objects/objects.R')
+
+# create directories for output
+file.loc <- paste0('../../output/rosby/')
+ifelse(!dir.exists(file.loc), dir.create(file.loc, recursive=TRUE), FALSE)
+
+# load data
+dat <- readRDS(paste0('../../output/prep_data/datus_state_rates_',year.start,'_',year.end))
+
+# split country into two halves
+dat.split <- readRDS(paste0())
+
+# make subnational summary for both sides of country
+
+# create correlation graph
+
