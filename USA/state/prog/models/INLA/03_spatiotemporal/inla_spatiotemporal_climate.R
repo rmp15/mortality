7b198185773baa97ab4e4cf13fe5710e204d3d03
Robbie Parks <r.parks15@imperial.ac.uk>
2017-04-03 16:11:48 +0100

    added stuff

--- USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate.R
+++ USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate.R
@@ -13,7 +13,6 @@ cluster.arg <- as.numeric(args[6])
 dname.arg <- as.character(args[7])
 metric.arg <- as.character(args[8])
 
-
 # types character for file strings
 types <- c('1','1a','2','2a','3','3a','4','1b','1c','1d','1e','1f')
 type.selected <- types[type.arg]
