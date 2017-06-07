9255338fc7f28fb182c60bca55b1a66443816a1b
rmp15 <r.parks15@imperial.ac.uk>
2017-06-07 12:47:33 +0100

    added stuff

---
+++ USA/state/prog/00_bash/inla_model_climate_backup_wrfstore5-10_wrfstore6-10.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# this script
+# backs up all the climate variable stuff from INLA models from wrfstore5-10 to wrfstore6-10
+
+rsync -avz -e ssh rmp15@wrfproc1-10.sp.ph.ic.ac.uk:~/data/* rmp15@wrfstore6-10.sp.ph.ic.ac.uk:/net/wrfstore6-10/disk1/rmp15/data/
