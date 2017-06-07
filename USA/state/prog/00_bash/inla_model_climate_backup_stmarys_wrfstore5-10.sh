9c0a918e35948d6bd1b5f2c0555b72937e259bee
rmp15 <r.parks15@imperial.ac.uk>
2017-06-07 14:09:12 +0100

    added stuff

--- USA/state/prog/00_bash/inla_model_climate_backup_stmarys_wrfstore5-10.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# this script
-# backs up all the climate variable stuff from INLA models from stmarys to wrfstore5-10
-
-00 01 * * * rsync -avz -e ssh /home/rmp15/data/mortality/US/state/climate_effects/* rmp15@wrfproc1-10.sp.ph.ic.ac.uk:~/data/mortality/US/state/climate_effects/
