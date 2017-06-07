9936e0c62ceb8ed4959d85cede907d6638f8ca16
Robbie Parks <r.parks15@imperial.ac.uk>
2017-06-07 14:18:20 +0100

    added crons

---
+++ USA/state/prog/02_cron/data_backup_stmarys_wrfstore5-10.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# this script
+# backs up all the climate variable stuff from INLA models from stmarys to wrfstore5-10
+
+#@weekly rsync -avz -e ssh /home/rmp15/data/* rmp15@wrfproc1-10.sp.ph.ic.ac.uk:~/data/
--- USA/state/prog/02_cron/inla_model_climate_backup_wrfstore5-10_wrfstore6-10.sh
+++ USA/state/prog/02_cron/inla_model_climate_backup_wrfstore5-10_wrfstore6-10.sh
@@ -3,4 +3,4 @@
 # this script
 # backs up all the climate variable stuff from INLA models from wrfstore5-10 to wrfstore6-10
 
-#@daily rsync -avz -e ssh rmp15@wrfproc1-10.sp.ph.ic.ac.uk:~/data/* rmp15@wrfstore6-10.sp.ph.ic.ac.uk:/net/wrfstore6-10/disk1/rmp15/data/
+#@daily rsync -avz -e ssh ~/data/* /net/wrfstore6-10/disk1/rmp15/data/
