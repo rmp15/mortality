08ae8eb415a220261a6cba36633d33b2753d0a43
rmp15 <r.parks15@imperial.ac.uk>
2017-12-10 15:55:44 +0000

    Subnational population in progress

--- USA/state/prog/data_explore/data_explore_cod.R
+++ USA/state/prog/data_explore/data_explore_cod.R
@@ -26,32 +26,30 @@ dat <- merge(dat,dat.year.month, by=c('year','month'))
 library(ggplot2)
 
 # graph data by age for a particular state cod and sex
-plot.state <- function(state=1,sex=1, cod='Cancer') {
-    ggplot(dat=subset(dat,fips==state&sex==sex&cod==cod))+
-    	geom_line(aes(x=year.month,y=100000*rate.adj),color='forestgreen') +
+plot.state <- function(state=1,sex=1) {
+    ggplot(dat=subset(dat,fips==state&sex==sex))+
+    	geom_line(aes(x=year.month,y=100000*rate.adj,color=cause)) +
     	xlab(label='Time') +
     	ylab(label='Mortality rate per 100,000') +
-    	ggtitle(paste0(state.lookup[state.lookup$fips==state,][[1]],', ',gender.lookup[sex],' ',cod,': mortality rates by agegroup')) +
-		#facet_wrap(~age) +
-		facet_wrap(~age, scale='free') +
+    	ggtitle(paste0(state.lookup[state.lookup$fips==state,][[1]],', ',gender.lookup[sex],' : mortality rates by age group and cause of death')) +
+		facet_wrap(~age,scale='free') +
     	scale_colour_brewer(palette = "Set3") +
-    	theme_bw()
+        theme(text = element_text(size = 15),panel.grid.major = element_blank(), panel.grid.minor = element_blank(), axis.text.x = element_text(angle=90), plot.title = element_text(hjust = 0.5),
+        panel.background = element_blank(),strip.background = element_blank(), axis.line = element_line(colour = "black"),legend.position = 'bottom',legend.justification='center',legend.background = element_rect(fill="gray90", size=.5, linetype="dotted"))
 }
-
-# CURRENTLY STOPPED HERE! PROGRESS FROM HERE
 
 # create output directory
 ifelse(!dir.exists("../../output/data_explore_cod"), dir.create("../../output/data_explore_cod"), FALSE)
 
 # plot all states for males
-pdf(paste0('../../output/data_explore/states_by_age_male_',year.start.arg,'_',year.end.arg,'.pdf'),paper='a4r',width=0,height=0)
+pdf(paste0('../../output/data_explore_cod/states_by_age_male_',year.start.arg,'_',year.end.arg,'.pdf'),paper='a4r',width=0,height=0)
 for (i in state.lookup$fips) {
 	print(plot.state(i,1))
 }
 dev.off()
 
 # plot all states for females
-pdf(paste0('../../output/data_explore/states_by_age_female_',year.start.arg,'_',year.end.arg,'.pdf'),paper='a4r',width=0,height=0)
+pdf(paste0('../../output/data_explore_cod/states_by_age_female_',year.start.arg,'_',year.end.arg,'.pdf'),paper='a4r',width=0,height=0)
 for (i in state.lookup$fips) {
 	print(plot.state(i,2))
 }
@@ -62,24 +60,25 @@ plot.age <- function(age=1,sex=1) {
 	dat$fips <- as.factor(dat$fips)
 	levels(dat$fips) <- state.lookup$full_name
    	ggplot(dat[dat$age==age.lookup[age] & dat$sex==sex,],aes(x=year.month)) +
-    	geom_line(aes(y=rate.adj),color='forestgreen') +
-    	xlab(label='time') +
-    	ylab(label='mortality rate') +
+    	geom_line(aes(y=rate.adj*100000,color=cause),) +
+    	xlab(label='Time') +
+    	ylab(label='Mortality rate per 100,000') +
     	ggtitle(paste0(age.lookup[age],', ',gender.lookup[sex],': mortality rates by agegroup')) +
     	facet_wrap(~fips) +
     	scale_colour_brewer(palette = "Set3") +
-    	theme_bw()
+        theme(text = element_text(size = 15),panel.grid.major = element_blank(), panel.grid.minor = element_blank(), axis.text.x = element_text(angle=90), plot.title = element_text(hjust = 0.5),
+        panel.background = element_blank(),strip.background = element_blank(), axis.line = element_line(colour = "black"),legend.position = 'bottom',legend.justification='center',legend.background = element_rect(fill="gray90", size=.5, linetype="dotted"))
 }
 
 # plot all ages for males
-pdf(paste0('../../output/data_explore/age_by_state_male_',year.start.arg,'_',year.end.arg,'.pdf'),paper='a4r',width=0,height=0)
+pdf(paste0('../../output/data_explore_cod/age_by_state_male_',year.start.arg,'_',year.end.arg,'.pdf'),paper='a4r',width=0,height=0)
 for (i in seq(length(age.lookup))) {
 	print(plot.age(i,1))
 }
 dev.off()
 
 # plot all ages for females
-pdf(paste0('../../output/data_explore/age_by_state_female_',year.start.arg,'_',year.end.arg,'.pdf'),paper='a4r',width=0,height=0)
+pdf(paste0('../../output/data_explore_cod/age_by_state_female_',year.start.arg,'_',year.end.arg,'.pdf'),paper='a4r',width=0,height=0)
 for (i in seq(length(age.lookup))) {
 	print(plot.age(i,2))
 }
