ccb11eabec7eecffcf45c39441d3dab3ab32993e
rmp15 <r.parks15@imperial.ac.uk>
2017-12-12 21:56:04 +0000

    updated wavelet title

--- USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_cod_fast.R
+++ USA/state/prog/models/INLA/03_spatiotemporal/inla_spatiotemporal_climate_cod_fast.R
@@ -20,7 +20,7 @@ cod.arg <- as.character(args[11])
 types <- c('1','1a','2','2a','3','3a','4','1b','1c','1d','1e','1f','1de','1ef','1g','0','minus1')
 type.selected <- types[type.arg]
 
-print(paste(age.arg,sex.arg,type.selected))
+print(paste(age.arg,sex.arg,type.selected,cod.arg))
 
 # range of years
 years <- year.start.arg:year.end.arg
