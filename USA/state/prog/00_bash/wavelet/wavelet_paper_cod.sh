646da14c077325c5d3498b146d49349885c64a8b
rmp15 <r.parks15@imperial.ac.uk>
2018-06-10 20:36:58 +0100

    Add eLife COD coding

--- USA/state/prog/00_bash/wavelet/wavelet_paper_cod.sh
+++ USA/state/prog/00_bash/wavelet/wavelet_paper_cod.sh
@@ -20,8 +20,8 @@ declare -a numsims=(10)
 declare -i sig=5
 declare -a noises=(1)
 #declare -a noises=(1 2)
-declare -a cods=("Substance_use_disorders")
-#declare -a cods=("AllCause" "Cancer" "Cardiopulmonary" "External" "Cardiovascular" "Chronic_respiratory_diseases" "Respiratory_infections" "Endocrine_disorders" "Genitourinary_diseases" "Maternal_conditions" "Neuropsychiatric_disorders" "Perinatal_conditions" "Substance_use_disorders")
+#declare -a cods=("Substance_use_disorders")
+declare -a cods=("AllCause" "Cancer" "Cardiopulmonary" "External" "Cardiovascular" "Chronic_respiratory_diseases" "Respiratory_infections" "Endocrine_disorders" "Genitourinary_diseases" "Maternal_conditions" "Neuropsychiatric_disorders" "Perinatal_conditions" "Substance_use_disorders")
 declare -a logs=(0)
 
 for noise in "${noises[@]}"; do
@@ -52,10 +52,10 @@ for cod in "${cods[@]}"; do
 echo "starting nationalised COM analysis for ${sexstrings[$sex-1]} $age, years $start - $end";
 
 # runs COM analysis
-Rscript ~/git/mortality/USA/state/prog/com/com_analysis_national_cod.R $start $end $age $sex $cod &
+#Rscript ~/git/mortality/USA/state/prog/com/com_analysis_national_cod.R $start $end $age $sex $cod &
 
 # runs anti-COM analysis
-Rscript ~/git/mortality/USA/state/prog/com/anti_com_analysis_national_cod.R $start $end $age $sex $cod &
+#Rscript ~/git/mortality/USA/state/prog/com/anti_com_analysis_national_cod.R $start $end $age $sex $cod &
 
 done; done; done;
 
@@ -88,10 +88,11 @@ for cod in "${cods[@]}"; do
 
 echo "plotting COM analysis for $country for $cod, years $start - $end";
 
-Rscript ~/git/mortality/USA/state/prog/com/com_data_process_cod.R $start $end $cod
+# processes com data
+#Rscript ~/git/mortality/USA/state/prog/com/com_data_process_cod.R $start $end $cod
 
 # plots
-#Rscript ~/git/mortality/USA/state/prog/com/com_plot_cod.R $start $end $cod
+Rscript ~/git/mortality/USA/state/prog/com/com_plot_cod.R $start $end $cod
 
 done;
 
