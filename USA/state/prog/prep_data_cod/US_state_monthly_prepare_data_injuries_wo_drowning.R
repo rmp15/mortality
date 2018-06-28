9525ce2311f7bb17bc600bcb2e4c5010d298e1b4
rmp15 <r.parks15@imperial.ac.uk>
2018-06-28 20:31:03 +0100

    Edited bash file to describe what's going on a bit more for eLife revisions

---
+++ USA/state/prog/prep_data_cod/US_state_monthly_prepare_data_injuries_wo_drowning.R
@@ -0,0 +1,208 @@
+rm(list=ls())
+
+# break down the arguments from Rscript
+args <- commandArgs(trailingOnly=TRUE)
+year.start.arg <- as.numeric(args[1])
+year.end.arg <- as.numeric(args[2])
+
+library(dplyr)
+library(plyr)
+library(foreign)
+
+# source only the intented variable
+source('../../data/objects/objects.R')
+rm(list=setdiff(ls(), c("year.start.arg","year.end.arg","icd9.lookup","icd10.lookup","cod.lookup.10")))
+
+# Function to summarise a year's data. x is the year in 2 number form (e.g. 1989 -> 89).
+# y is the number of rows. default (-1) is all rows.
+yearsummary_cod  <- function(x=2000) {
+
+	print(paste0('year ',x,' now being processed'))
+  
+	# load year of deaths
+	file.loc <- '~/data/mortality/US/state/processed/cod/'
+	dat.name <- paste0(file.loc,"deathscod",x,".dta")
+	dat <- read.dta(dat.name)
+
+	# fix sex classification if in certain years
+	if(x %in% c(2003:2010,2012)){
+		dat$sex = plyr::mapvalues(dat$sex,from=sort(unique(dat$sex)),to=c(2,1))
+	}
+
+	# load lookup for fips
+	dat$fips = substr(dat$fips,1,2)
+	dat$fips <- as.numeric(dat$fips)
+
+	# add extra label for CODs based on relevant ICD year
+	start_year = 1999
+	if(x<start_year) {
+        # ICD 9 coding for broad cod coding
+		dat$cause[nchar(dat$cause)==3] <- paste0(dat$cause[nchar(dat$cause)==3],'0')
+		dat$cause.numeric = as.numeric(dat$cause)
+		dat$cause.group = 	ifelse(dat$cause.numeric>=1400&dat$cause.numeric<=2399,'Cancer',
+							ifelse(dat$cause.numeric>=3900&dat$cause.numeric<=5199,'Cardiopulmonary',
+							ifelse(dat$cause.numeric>=8000&dat$cause.numeric<=9999,'External',
+							'Other')))
+
+        dat$cause.group = as.character(dat$cause.group)
+
+        # move deaths due to weather-based heat/cold to 'Other'
+        dat$cause.group = ifelse(as.numeric(substr(dat$cause.numeric,1,3))==900|as.numeric(substr(dat$cause.numeric,1,3))==901,'Other',dat$cause.group)
+
+		dat.merged = dat
+
+        # only filter for external
+        dat.merged = subset(dat.merged,cause.group=='External')
+        dat.merged$cause.group = NULL
+
+		# merge cod in ICD 9 coding
+		icd9.lookup$cause = as.numeric(icd9.lookup$cause)
+		dat.merged = merge(dat.merged,icd9.lookup,by='cause',all.x=1)
+        dat.merged$cause.group = as.character(dat.merged$cause.group)
+        dat.merged$cause.group = ifelse(is.na(dat.merged$cause.group)==TRUE,'Other',dat.merged$cause.group)
+
+		# remove drowning deaths from unintentional
+		dat.merged$cause.group = ifelse(dat.merged$cause.numeric>=9100&dat.merged$cause.numeric<=9109, 'Accidental drowning and submersion',dat.merged$cause.group)
+
+	}
+	if(x>=start_year){
+        # merge cod in ICD 10 coding for broad letter coding
+        dat$cause[nchar(dat$cause)==3] <- paste0(dat$cause[nchar(dat$cause)==3],'0')
+		dat$letter = substr(dat$cause,1,1)
+		dat.merged = merge(dat,cod.lookup.10,by.x='letter',by.y='letter',all.x=1)
+
+        dat.merged$cause.group = as.character(dat.merged$cause.group)
+
+        # move deaths due to weather-based heat/cold to 'Other'
+        dat.merged$cause.group = ifelse((dat.merged$cause=='X300'|dat.merged$cause=='X310'),'Other',as.character(dat.merged$cause.group))
+
+		# move contraversial deaths due to drugs to 'Other'
+        dat.merged$cause.group = ifelse((dat.merged$cause=='X410'|dat.merged$cause=='X420'|dat.merged$cause=='X450'|dat.merged$cause=='X490'),'Other',as.character(dat.merged$cause.group))
+
+        # only filter for external
+        dat.merged = subset(dat.merged,cause.group=='External')
+        dat.merged$cause.group = NULL
+
+		# merge cod in ICD 10 coding
+		dat.merged = merge(dat.merged,icd10.lookup,by='cause',all.x=1)
+        dat.merged$cause.group = as.character(dat.merged$cause.group)
+        dat.merged$cause.group = ifelse(is.na(dat.merged$cause.group)==TRUE,'Other',dat.merged$cause.group)
+
+		# remove drowning deaths from unintentional
+		dat.merged$cause.group = ifelse(dat.merged$letter=='W'&dat.merged$cause.numeric>=650&dat.merged$cause.numeric<=749,'Accidental drowning and submersion',dat.merged$cause.group)
+
+	}
+
+	# add agegroup groupings
+  	dat.merged$agegroup <-  
+              	ifelse (dat.merged$age<5,   0,
+                ifelse (dat.merged$age<15,  5,        
+                ifelse (dat.merged$age<25,  15, 
+                ifelse (dat.merged$age<35,  25, 
+                ifelse (dat.merged$age<45,  35, 
+                ifelse (dat.merged$age<55,  45, 
+                ifelse (dat.merged$age<65,  55, 
+                ifelse (dat.merged$age<75,  65, 
+                ifelse (dat.merged$age<85,  75,
+                   	85)))))))))
+
+	# summarise by state,year,month,sex,agegroup
+    dat.summarised <- dplyr::summarise(group_by(dat.merged,cause.group,fips,year,monthdth,sex,agegroup),deaths=sum(deaths))
+  	names(dat.summarised)[1:7] <- c('cause','fips','year','month','sex','age','deaths')
+	dat.summarised <- na.omit(dat.summarised)
+
+	# create an exhaustive list of location sex age month
+	fips 	=	c(1,2,4,5,6,8,9,10,11,12,13,15,16,17,18,19,20,21,22,23,24,25,
+				26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,
+				41,42,44,45,46,47,48,49,50,51,53,54,55,56)
+	month 	= 	c(1:12)
+	sex 	= 	c(1:2)
+	age 	= 	c(0,5,15,25,35,45,55,65,75,85)
+	cause 	=	c('Unintentional','Intentional')
+
+	complete.grid <- expand.grid(fips=fips,month=month,sex=sex,age=age,cause=cause)
+	complete.grid$year <- unique(dat.summarised$year)
+
+	# merge deaths counts with complete grid to ensure there are rows with zero deaths
+	dat.summarised.complete <- merge(complete.grid,dat.summarised,by=c('cause','fips','year','month','sex','age'),all.x='TRUE')
+
+	# assign missing deaths to have value 0
+	dat.summarised.complete$deaths <- ifelse(is.na(dat.summarised.complete$deaths)==TRUE,0,dat.summarised.complete$deaths)
+
+	# print statistics of sub-causes
+	print(ddply(dat.summarised.complete,.(cause),summarise,deaths=sum(deaths)))
+
+	print(paste0('total deaths in year ',sum(dat$deaths),', total deaths for injuries ',sum(dat.merged$deaths),' ',sum(dat.summarised.complete$deaths)))
+
+  	return(dat.summarised.complete)
+}
+
+# Function to append all the years desired to be summarised into one file
+appendyears  <- function(x=1980, y=1981) {
+  	years <- x:y
+  	z     <- x
+  	dat   <- data.frame()
+  	while (z %in% years) {
+    		dat <- rbind(dat, yearsummary_cod(z))
+    		print(paste0(z," done"))
+    		z <- z+1
+  	}
+  	return(dat)
+}
+
+# append summarised dataset
+dat.appended <- appendyears(year.start.arg,year.end.arg)
+
+# reorder appended data
+dat.appended <- dat.appended[order(dat.appended$cause,dat.appended$fips,dat.appended$sex,dat.appended$age,dat.appended$year,dat.appended$month),]
+
+# Filter missing age results and complete results
+dat.appended   <- na.omit(dat.appended)
+
+# Add USA label
+dat.appended$iso3 <- "USA"
+
+# add inferred population data by day
+pop.state <- readRDS('../../output/pop_us_infer/statePopulations_infer_by_days_new_years')
+pop.state$fips <- as.integer(pop.state$fips)
+
+# merge deaths and population files
+dat.merged <- merge(dat.appended,pop.state,by=c('sex','age','year','month','fips'))
+
+# reorder
+dat.merged <- dat.merged[order(dat.merged$cause,dat.merged$fips,dat.merged$sex,dat.merged$age,dat.merged$year,dat.merged$month),]
+
+# add rates
+dat.merged$rate <- dat.merged$deaths / dat.merged$pop
+dat.merged$rate.adj <- dat.merged$deaths / dat.merged$pop.adj
+
+# move old adjusted rate
+dat.merged$rate.adj.old <- dat.merged$rate.adj
+
+# leap year test
+is.leapyear=function(year){
+    return(((year %% 4 == 0) & (year %% 100 != 0)) | (year %% 400 == 0))
+}
+
+dat.merged$leap <- as.integer(is.leapyear(dat.merged$year))
+
+# adjust deaths to a 31-day month
+# 30-day months = April, June, September, November (4,6,9,11)
+# 31-day months = January, March, May, July, August, October, December (1,3,5,7,8,10,12)
+# 28/29-day months = Februray (2)
+dat.merged$deaths.adj <- ifelse(dat.merged$month %in% c(1,3,5,7,8,10,12), dat.merged$deaths,
+                  ifelse(dat.merged$month %in% c(4,6,9,11), dat.merged$deaths*(31/30),
+                  ifelse((dat.merged$month==2 & dat.merged$leap==0), dat.merged$deaths*(31/28),
+                  ifelse((dat.merged$month==2 & dat.merged$leap==1), dat.merged$deaths*(31/29),
+                  'ERROR'
+                  ))))
+dat.merged$deaths.adj <- as.numeric(dat.merged$deaths.adj)
+
+# calculate new rate.adj
+dat.merged$rate.adj <- dat.merged$deaths.adj / dat.merged$pop.adj
+
+# create output directory
+ifelse(!dir.exists("../../output/prep_data_cod"), dir.create("../../output/prep_data_cod"), FALSE)
+
+# output file as RDS
+saveRDS(dat.merged,paste0('../../output/prep_data_cod/datus_state_rates_cod_injuries_wo_drowning_ons_',year.start.arg,'_',year.end.arg))
