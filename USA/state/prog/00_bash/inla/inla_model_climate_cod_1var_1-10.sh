3025e65806b239cb67d2c7f0f867890af3a38536
rmp15 <r.parks15@imperial.ac.uk>
2018-07-09 00:21:01 +0100

    Fixed injury paper bash script

--- USA/state/prog/00_bash/inla/inla_model_climate_cod_1var_1-10.sh
+++ USA/state/prog/00_bash/inla/inla_model_climate_cod_1var_1-10.sh
@@ -18,7 +18,7 @@ declare -i end2=2016
 declare country="USA"
 declare dname="t2m"
 declare metric="meanc3"
-declare cod="Accidental_drowning_and_submersion"
+declare cod="Accidental_falls"
 declare -i fast=1
 declare -i contig=1
 
