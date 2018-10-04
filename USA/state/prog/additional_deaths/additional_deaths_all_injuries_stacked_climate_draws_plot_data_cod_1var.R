524962f022a0e7f7d611217e3e890891a95d85ef
rmp15 <rparks15@imperial.ac.uk>
2018-10-04 14:01:41 +0100

    added plots

--- USA/state/prog/additional_deaths/additional_deaths_all_injuries_stacked_climate_draws_plot_data_cod_1var.R
+++ USA/state/prog/additional_deaths/additional_deaths_all_injuries_stacked_climate_draws_plot_data_cod_1var.R
@@ -374,7 +374,7 @@ ggplot() +
     scale_y_continuous(labels=scales::percent) +
     scale_color_manual(values=colors.subinjuries[c(1,2,3,4,5,6)]) +
     # scale_y_continuous(breaks = seq(min.plot, max.plot, by = 50),limits=c(min.plot,max.plot)) +
-    guides(color=guide_legend(title="Subcategory of intentional injury",nrow=1)) +
+    guides(color=guide_legend(title="Subcategory of injury",nrow=1)) +
     # ggtitle('Additional deaths by types of intentional injuries') +
     theme_bw() + theme(text = element_text(size = 15),
     panel.grid.major = element_blank(),axis.text.x = element_text(angle=90),
