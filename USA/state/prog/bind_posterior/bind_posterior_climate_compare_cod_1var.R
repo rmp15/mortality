c40477215b504da02c443d6a3c9c45df863a832a
rmp15 <r.parks15@imperial.ac.uk>
2018-10-08 08:16:50 +0100

    updates to wavelet coding

---
+++ USA/state/prog/bind_posterior/bind_posterior_climate_compare_cod_1var.R
@@ -0,0 +1,67 @@
+rm(list=ls())
+
+# break down the arguments from Rscript
+args <- commandArgs(trailingOnly=TRUE)
+year.start <- as.numeric(args[1])
+year.end <- as.numeric(args[2])
+country <- as.character(args[3])
+model <- as.numeric(args[4])
+dname <- as.character(args[5])
+metric <- as.character(args[6])
+cause <- as.character(args[7]) ; cause <- gsub('_',' ',cause)
+contig.arg <- as.numeric(args[8])
+pw.arg <- as.numeric(args[9])
+
+library(ggplot2)
+
+# lookups
+source('../../data/objects/objects.R')
+model <- models[model]
+
+# create directories for output
+file.loc <- paste0('../../output/compare_posterior_climate/',year.start,'_',year.end,
+'/',dname,'/',metric,'/non_pw/')
+if(contig==1){
+    file.loc <- paste0('../../output/compare_posterior_climate/',year.start,'_',year.end,
+'/',dname,'/',metric,'/non_pw/type_',model,'/parameters/contig/',cause,'/')
+}
+ifelse(!dir.exists(file.loc), dir.create(file.loc,recursive=TRUE), FALSE)
+
+
+# MODEL 1D
+file.name <- paste0('~/data/mortality/US/state/climate_effects/',dname,'/',metric,'/non_pw/type_1d/age_groups/85/',country,'_rate_pred_type1d_85_Men_',year.start,'_',year.end,'_',dname,'_',metric,'_',cause,'_parameters_fast_contig')
+model.current <- readRDS(file.name)
+model.1d <- model.current$summary.random$month5
+model.1d$model = 'Base'
+model.1d = as.data.frame(model.1d[,c('mean')])
+names(model.1d) = c('mean.base')
+
+# MODEL 1D5
+file.name <- paste0('~/data/mortality/US/state/climate_effects/',dname,'/',metric,'/non_pw/type_1d5/age_groups/85/',country,'_rate_pred_type1d5_85_Men_',year.start,'_',year.end,'_',dname,'_',metric,'_',cause,'_parameters_fast_contig')
+model.current <- readRDS(file.name)
+model.1d5 <- model.current$summary.random$month5
+model.1d5$model = 'Hyper edit 2'
+model.1d5 = as.data.frame(model.1d5[,c('mean')])
+names(model.1d5) = c('mean.hyper.edit.2')
+
+# MODEL 1D6
+file.name <- paste0('~/data/mortality/US/state/climate_effects/',dname,'/',metric,'/non_pw/type_1d6/age_groups/85/',country,'_rate_pred_type1d6_85_Men_',year.start,'_',year.end,'_',dname,'_',metric,'_',cause,'_parameters_fast_contig')
+model.current <- readRDS(file.name)
+model.1d6 <- model.current$summary.random$month5
+model.1d6$model = 'Hyper edit 1'
+model.1d6 = as.data.frame(model.1d6[,c('mean')])
+names(model.1d6) = c('mean.hyper.edit.1')
+
+
+# MODEL 1D7
+file.name <- paste0('~/data/mortality/US/state/climate_effects/',dname,'/',metric,'/non_pw/type_1d7/age_groups/85/',country,'_rate_pred_type1d7_85_Men_',year.start,'_',year.end,'_',dname,'_',metric,'_',cause,'_parameters_fast_contig')
+model.current <- readRDS(file.name)
+model.1d7 <- model.current$summary.random$month5
+model.1d7$model = 'Hyper edit 3'
+model.1d7 = as.data.frame(model.1d7[,c('mean')])
+names(model.1d7) = c('mean.hyper.edit.3')
+
+models = cbind(model.1d,model.1d5,model.1d6,model.1d7)
+
+# plots against each other
+plot(models)
--- USA/state/prog/fitted_against_raw/fitted_against_raw_cod_1var.R
+++ USA/state/prog/fitted_against_raw/fitted_against_raw_cod_1var.R
@@ -41,8 +41,8 @@ colorway = c("navy","deepskyblue2","deepskyblue3","lightgreen","white","gold","o
 dat.all = data.frame()
 if(contig==1){
     if(cause!='AllCause'){
-        # for(i in c(0,5,15,25,35,45,55,65,75,85)){
-        for(i in c(85)){
+        for(i in c(0,5,15,25,35,45,55,65,75,85)){
+        # for(i in c(85)){
             for(j in c('Men','Women')){
         dat <- readRDS(paste0('~/data/mortality/US/state/climate_effects/',dname,'/',metric,'/non_pw/type_',model,
         '/age_groups/',i,'/',
@@ -156,8 +156,8 @@ levels(dat.all$fips) <- state.lookup$full_name
 
 # plotting by age over time per state
 pdf(paste0(file.loc,'raw_against_adjusted_over_time_males_',model,'_',year.start,'_',year.end,'_',dname,'_',metric,'_',cause,'.pdf'),paper='a4r',height=0,width=0)
-# for(i in c(0,5,15,25,35,45,55,65,75,85)){
-for(i in c(85)){
+for(i in c(0,5,15,25,35,45,55,65,75,85)){
+# for(i in c(85)){
     print(
         ggplot(data=subset(dat.all,sex==1&year.month<=600&age==i)) +
         geom_point(aes(x=year.month,y=rate.adj),color='red') +
@@ -168,8 +168,8 @@ for(i in c(85)){
 dev.off()
 
 pdf(paste0(file.loc,'raw_against_adjusted_over_time_females_',model,'_',year.start,'_',year.end,'_',dname,'_',metric,'_',cause,'.pdf'),paper='a4r',height=0,width=0)
-# for(i in c(0,5,15,25,35,45,55,65,75,85)){
-for(i in c(85)){
+for(i in c(0,5,15,25,35,45,55,65,75,85)){
+# for(i in c(85)){
     print(
         ggplot(data=subset(dat.all,sex==1&year.month<=600&age==i)) +
         geom_point(aes(x=year.month,y=rate.adj),color='red') +
