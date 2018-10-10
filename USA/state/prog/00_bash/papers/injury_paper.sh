d1bb743ed5563cedc3840472451b1122769369d2
rmp15 <rparks15@imperial.ac.uk>
2018-10-10 12:26:35 +0100

    updates to wavelet coding

--- USA/state/prog/00_bash/papers/injury_paper.sh
+++ USA/state/prog/00_bash/papers/injury_paper.sh
@@ -13,6 +13,7 @@ declare -a sexstrings=('male' 'female')
 declare -a model=(10)
 declare -i start=1980
 declare -i end=2016
+declare -i start2=1979
 declare country="USA"
 declare dname="t2m"
 declare metric="meanc3"
@@ -20,30 +21,34 @@ declare -i fast=1
 declare -i contig=1
 declare -a draws=(5000)
 
-# to correct directory
-cd ~/git/mortality/USA/state/prog/00_bash/
-
 #################################################
 # Figures 1 and 2
 #################################################
 
+# to correct directory
+cd ~/git/mortality/USA/state/prog/00_bash/
+
 echo "plotting figures 1 and 2 for injury paper $start - $end";
 
 ## runs code
-Rscript ~/git/mortality/USA/state/prog/data_explore/data_explore_injuries_ons_subcod.R $start $end
+#Rscript ~/git/mortality/USA/state/prog/data_explore/data_explore_injuries_ons_subcod.R $start $end
 
 #################################################
 # Figure 3
 #################################################
 
+# to correct directory
+cd ~/git/climate/countries/USA/prog/00_bash/
 
 echo "plotting figure 3 injury paper $start - $end";
 
-
-#Rscript ~/git/climate/countries/USA/prog/06_plots/plots_against_time.R $start $end $dname $metric
+#Rscript ~/git/climate/countries/USA/prog/06_plots/plots_against_time.R $start2 $end $dname $metric
 
 #################################################
 # Figure 4 and supplementary figure 1
 #################################################
+
+# to correct directory
+cd ~/git/climate/countries/USA/prog/00_bash/
 
 #Rscript ~/git/mortality/USA/state/prog/additional_deaths/additional_deaths_all_injuries_stacked_climate_draws_plot_data_cod_1var.R $start $end $country $model $dname $metric $contig $draws
