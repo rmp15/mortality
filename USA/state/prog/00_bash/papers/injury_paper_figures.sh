a006f1341a51ec79b4013e98a9e8b53cfc4ca8b4
rmp15 <rparks15@imperial.ac.uk>
2019-02-19 16:18:09 +0000

    temperature correlation bash update

--- USA/state/prog/00_bash/papers/injury_paper_figures.sh
+++ USA/state/prog/00_bash/papers/injury_paper_figures.sh
@@ -30,7 +30,7 @@ cd ~/git/mortality/USA/state/prog/00_bash/
 echo "plotting figures 1 and 2 for injury paper $start - $end";
 
 # runs code
-Rscript ~/git/mortality/USA/state/prog/data_explore/data_explore_injuries_ons_subcod.R $start $end
+#Rscript ~/git/mortality/USA/state/prog/data_explore/data_explore_injuries_ons_subcod.R $start $end
 
 # old (no need to run but left just in case want to summarise by intentional and unintentional)
 #Rscript ~/git/mortality/USA/state/prog/data_explore/data_explore_injuries_ons.R $start $end
@@ -59,6 +59,17 @@ echo "plotting figure 4 and supplementary figure injury paper $start - $end";
 cd ~/git/mortality/USA/state/prog/00_bash/
 
 #Rscript ~/git/mortality/USA/state/prog/additional_deaths/additional_deaths_all_injuries_stacked_climate_draws_plot_data_cod_1var.R $start $end $country $model $dname $metric $contig $draws
+
+#################################################
+# Supplementary table 1
+#################################################
+
+# to correct directory
+cd ~/git/climate/countries/USA/prog/00_bash/
+
+echo "plotting supplementary table 1 injury paper $start - $end";
+
+Rscript ~/git/climate/countries/USA/prog/14_comparing_max_min/comparing_max_min.R $start2 $end 'mean' $dname
 
 #################################################
 # Tables XX-XX (not currently in paper)
